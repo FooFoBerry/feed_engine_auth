3eca89f5003312a88bc0de0b8121b8f27a965cc1
Simon Taranto <simon.taranto@gmail.com>
2014-01-07 14:05:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeedEngineAuth::Application.config.secret_key_base = 'add7b720d20122b859ae8d3ad0e3561fb27ac39719b554e6524899b5a1f5e0d8a885e0e4ec59a89d081970c789ba45cba65b37236fdb03fd20f7cda8a6f12770'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FeedEngineAuth::Application.config.session_store :cookie_store, key: '_feed_engine_auth_session'
