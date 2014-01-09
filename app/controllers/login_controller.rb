a0e28767043f677f689b9926149aebcdd8811c04
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-09 11:30:59 -0700

    set redirect back to root if no GitHub hash

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -6,29 +6,22 @@ class LoginController < ApplicationController
   end
 
   def create
-    hash = auth_hash
-    user = User.find_or_create_by(:uid => hash["uid"])
-    cookies[:uid] = hash["uid"]
-    cookies[:user_id] = user.id
-    redirect_to dashboard_path
+    user = User.find_or_create_by(:uid => auth_hash["uid"]) if auth_hash
+    if user
+      cookies[:uid]     = user.uid
+      cookies[:user_id] = user.id
+      cookies[:flash]   = "Successfully Signed In!"
+      redirect_to dashboard_path
+    else
+      flash[:notice] = "Sorry, something went wrong."
+      redirect_to root_path
+    end
   end
 
   def auth_hash
     request.env['omniauth.auth']
   end
 
-  def post(param)
-
-  end
-
-end
-
-class Foofoberry
-  #this will be the gem  
-  def self.find_or_create_user(user_data)
-    "12"
-    #this will be a post method of some sort
-  end   
 end
 
 
--- spec/features/user_can_view_login_page_spec.rb
+++ spec/features/user_can_view_login_page_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe "the user viewing the homepage" do
-  it "can view login button" do
+
+  it "can view login button and sign up" do
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      :provider => 'github',
+      :uid => '123545'
+    })
     visit root_path
     expect(page).to have_link("Get started with Github",
                               :href => "/auth/github")
     click_on 'Get started with Github'
     expect(page.current_url).to eq dashboard_url
   end
+
 end
