9dfcbdcb797f5856be20a89ac3f249cd5fa0b99a
Simon Taranto <simon.taranto@gmail.com>
2014-01-16 09:54:01 -0700

    update omniauth configs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, '24fb172060ed0abed1bb', '909c8386e21c199fcd79589f306ddb4d6d8e642f'
 end
+
+OmniAuth.config.full_host = "http://localhost:8080"
